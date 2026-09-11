7c020741a3a3a4062fb97aa3d97b7e40c0846bad
sukudesiya <your-github-email@example.com>
2026-09-11 04:52:58 -0400

    Fix logout route in djangoapp/urls.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -41,6 +41,11 @@ def login_user(request):
         data = {"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
 
+def logout_request(request):
+    logout(request)  # Terminate user session
+    data = {"userName": ""}  # Return empty username
+    return JsonResponse(data)
+
 # Create a `logout_request` view to handle sign out request
 # def logout_request(request):
 # ...
