e408d8845fa9e0f84259ee278e8a13eeefa854d3
sukudesiya <your-github-email@example.com>
2026-09-11 08:12:11 -0400

    Add CarMake/CarModel models, admin registration, and get_cars view

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,23 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
-# CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 1
 
-# CarModelAdmin class
 
-# CarMakeAdmin class with CarModelInline
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year', 'dealer_id')
+
+
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ('name', 'description')
+
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -9,6 +9,8 @@ from django.contrib import messages
 from datetime import datetime
 from django.contrib.auth import authenticate, login
 from django.http import JsonResponse
+from .models import CarMake, CarModel
+from .populate import initiate
 import json
 
 from django.http import JsonResponse
@@ -73,6 +75,17 @@ def registration(request):
     else:
         data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
+
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if count == 0:
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels": cars})
 # Create a `logout_request` view to handle sign out request
 # def logout_request(request):
 # ...
